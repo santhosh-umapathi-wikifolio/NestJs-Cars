48dcf3f4f2dd5a87eea3419d9133dde73eb336cd
Santhosh Umapathi <santhosh.umapathi@wikifolio.com>
2025-12-11 11:23:22 +0100

    base project setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
 }
-bootstrap();
+
+
+bootstrap().then(() => {
+  console.log(`Application is running on: http://localhost:${PORT}`);
+}).catch((err) => {
+  console.error('Error during application bootstrap:', err);
+});
