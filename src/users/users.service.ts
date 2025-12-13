42adfa439c6f6f9999e43997e6f2f6bbf0cd834f
Santhosh Umapathi <santhosh.umapathi@wikifolio.com>
2025-12-13 06:35:47 +0100

    Add user retrieval and management methods to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { Repository } from 'typeorm';
@@ -11,4 +11,32 @@ export class UsersService {
         const user = this.usersRepository.create({ email, password });
         return this.usersRepository.save(user);
     }
+
+    findOne(id: number) {
+        return this.usersRepository.findOneBy({ id });
+    }
+
+    find(email: string) {
+        return this.usersRepository.find({ where: { email } });
+    }
+
+    async updateUser(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        const { id: _, ...updateAttrs } = attrs; // Exclude id
+        Object.assign(user, updateAttrs);
+        return this.usersRepository.save(user);
+    }
+
+    async removeUser(id: number) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        return this.usersRepository.remove(user);
+    }
 }
+
+
