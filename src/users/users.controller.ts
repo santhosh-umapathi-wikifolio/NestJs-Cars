a04236dbf1c99d466a98423580a18c23f021982d
Santhosh Umapathi <santhosh.umapathi@wikifolio.com>
2025-12-14 21:02:29 +0100

    Refactor UsersController to utilize AuthService for user signup functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,16 +4,17 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth/auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(private readonly usersService: UsersService, private readonly authService: AuthService) { }
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         const { email, password } = body;
-        return this.usersService.createUser(email, password);
+        return this.authService.signup(email, password);
     }
 
     @Get('/:id')
