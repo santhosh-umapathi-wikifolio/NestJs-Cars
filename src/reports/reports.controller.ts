a7fb4feb5156bd45c9d3e24909c79d7681a19b74
Santhosh Umapathi <santhosh.umapathi@wikifolio.com>
2025-12-16 16:14:21 +0100

    feat: update reports functionality to include user context
    - Modified ReportsController to include CurrentUser decorator for user context in createReport method.
    - Updated ReportsService to associate the created report with the current user.
    - Adjusted flashpostCollections.db and flashpostCookies.db to reflect changes in user-related data.
    - Updated binary db.sqlite file.

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,15 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
-import { authGuard, AuthGuard } from 'src/guards/auth.guard';
+import { authGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/users.entity';
 
 @Controller('reports')
-@authGuard()
+
 export class ReportsController {
     constructor(private readonly reportsService: ReportsService) { }
 
     @Post('/create')
-    createReport(@Body() body: CreateReportDto) {
-        return this.reportsService.create(body);
+    @authGuard()
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+        return this.reportsService.create(body, user);
     }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/users.entity';
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(Report) private reportsRepository: Repository<Report>) { }
 
-    create(data: Partial<Report>) {
+    create(data: Partial<Report>, user: User) {
         const report = this.reportsRepository.create(data);
+        report.user = user;
         return this.reportsRepository.save(report);
     }
 }
