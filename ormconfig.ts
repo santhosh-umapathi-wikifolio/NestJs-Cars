40f00f950e37bd36b342d7db39e5ac762cb7e092
Santhosh Umapathi <santhosh.umapathi@wikifolio.com>
2025-12-17 14:08:38 +0100

    feat(config): update environment configuration for database and load .env files based on NODE_ENV

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,8 +1,27 @@
+import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
+
+// Load environment-specific .env file
+let envFile: string;
+switch (process.env.NODE_ENV) {
+  case 'production':
+    envFile = '.env.production';
+    break;
+  case 'test':
+    envFile = '.env.test';
+    break;
+  case 'development':
+  default:
+    envFile = '.env.development';
+    break;
+}
+
+// Load the environment variables from the specified .env file
+config({ path: envFile });
 
 const dbConfig: DataSourceOptions = {
   type: 'sqlite',
-  database: 'db.sqlite',
+  database: process.env.DB_NAME || 'db.sqlite',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production', // Only use synchronize: true in development
 };
